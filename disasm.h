e8b77238d460efd8da63b14fc33cf989afbbf279
Drakeor <drakeor.dragon@gmail.com>
2018-09-09 21:40:41 -0700

    TravisCI

--- disasm.h
+++ disasm.h
@@ -1,7 +1,7 @@
 #ifndef DISASM_H
 #define DIsASM_h
 
-void OpToText(unsigned char* buffer, int* counter, char* msg, char msg_len);
+void OpToText(unsigned char* buffer, int* counter);
 void Disassemble(unsigned char* buffer, int buffer_size);
 
 #endif
