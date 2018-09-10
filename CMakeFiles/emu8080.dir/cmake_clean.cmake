f7f684a64e6eb6d8a9990a82dcc1e6421c833f52
Drakeor <drakeor.dragon@gmail.com>
2018-09-09 21:09:59 -0700

    Adding Travis-CI script

---
+++ CMakeFiles/emu8080.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/emu8080.dir/main.c.o"
+  "emu8080.pdb"
+  "emu8080"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/emu8080.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
